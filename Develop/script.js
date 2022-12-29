a1f4049c6eb27973939dbfe69d0e26cfde321369
TLeeming15 <tyler.leeming21@gmail.com>
2022-12-29 08:02:07 -0500

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,21 @@
 // Assignment code here
+function generatePassword (){
+
+var charactersArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
-// Get references to the #generate element
+
+
+
+
+generateBtn.addEventListener("click",generatePassword);
+
+// Get references to the #generate elemen
 var generateBtn = document.querySelector("#generate");
-
+var password = document.querySelector("#password");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
