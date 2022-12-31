df3b83948b40bbe9b2e632cad56f32b33f72322d
TLeeming15 <tyler.leeming21@gmail.com>
2022-12-31 13:07:10 -0500

    added comments to the script

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,21 @@
-
+// These are emtpy arrays to store data in later
 var passwordLength = [];
 var choiceArray=[];
-
+// These are arrays separated into logical categories of characters.Each array holds all the possible characters for each category. These are linked with the prompts that the user will interact with to customize their unique password.
 var charactersArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
+ 
 var generateBtn = document.querySelector("#generate")
-
+// When the user clicks the button the writePassword function will be called
 generateBtn.addEventListener("click",writePassword);
-
+// These are the prompts that must be returend true in order for the writePassword function to run.
 function passwordPrompts() {
 
-  choiceArray = [];
-
+// These are all the prompts that the user will interact with to customize their password. The first is a string and the rest are Boolean.
   passwordLength = parseInt(prompt("How many characters would you like your password to be? It must be between 8 and 128."));
-
+// If the user types anything that is Not a Number (NaN then it will return false and addtional prompt will be sent to the user to start over.)
   if (isNaN(passwordLength) || passwordLength < 7 || passwordLength > 128){
     alert ("Password length must be entered in digit form and must be a minimum of 8 characters or maximum of 128 characters")
     return false;
@@ -41,18 +40,18 @@ function passwordPrompts() {
   return true;
 }
 
-// Write password to the #password input
+// Writes password to the #password input. Also ensures that this only happens when the prompts are returned true.
 function writePassword() {
   var properPrompts = passwordPrompts();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+  // This calls the generate password function.
   if (properPrompts){
     var password = generatePassword();
     passwordText.value = password;
   }
 }
-
+// This function actually generates the password using the data stored in the arrays that are chosen throught he user prompts.
 function generatePassword() {
 var password = "";
   for(var i = 0; i < passwordLength; i++){
